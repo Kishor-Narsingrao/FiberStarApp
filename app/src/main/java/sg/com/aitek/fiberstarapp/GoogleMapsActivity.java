0fb8620b7cd66c0764929c57daa53f82da86d025
Kishor <kishor.narsingrao@c2dsoft.com>
2017-05-22 17:41:39 +0530

    Solving "LDAP/AD not authenticated  or failed" Error. Attachment Compatible for android nougat. place multiple marker on same loction in google map. Add different image for each entity.

--- app/src/main/java/sg/com/aitek/fiberstarapp/GoogleMapsActivity.java
+++ app/src/main/java/sg/com/aitek/fiberstarapp/GoogleMapsActivity.java
@@ -136,21 +136,12 @@ public class GoogleMapsActivity extends FragmentActivity
             if (ActivityCompat.shouldShowRequestPermissionRationale(GoogleMapsActivity.this,
                     android.Manifest.permission.READ_EXTERNAL_STORAGE)) {
 
-                // Show an explanation to the user *asynchronously* -- don't block
-                // this thread waiting for the user's response! After the user
-                // sees the explanation, try again to request the permission.
-
             } else {
-
-                // No explanation needed, we can request the permission.
 
                 ActivityCompat.requestPermissions(GoogleMapsActivity.this,
                         new String[]{android.Manifest.permission.READ_EXTERNAL_STORAGE},
                         MY_PERMISSIONS_REQUEST_READ_CONTACTS);
 
-                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
-                // app-defined int constant. The callback method gets the
-                // result of the request.
             }
         }
 
